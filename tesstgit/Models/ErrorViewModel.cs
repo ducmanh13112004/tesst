f27163bc9a60821210292473f93f04ca29879146
ducmanh13112004 <tmanh5417@gmail.com>
2024-12-06 20:52:56 +0700

    tests git

---
+++ tesstgit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tesstgit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
